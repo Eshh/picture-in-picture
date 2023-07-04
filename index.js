c888dbedec2c6c3b1e912d11d8b0a55d2233b13f
eswarprasadk <eswarprasadk@turito.com>
2023-07-04 15:14:23 +0530

    PIP logic

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-alert("came");
+const videoRef = document.getElementById("video");
+const btnRef = document.getElementById("btn");
+
+// Prompt user to select video stream
+
+async function selectMediaStream() {
+  try {
+    const media = await navigator.mediaDevices.getDisplayMedia();
+    videoRef.srcObject = media;
+    videoRef.onloadedmetadata = () => {
+      videoRef.hidden = false;
+      videoRef.play();
+    };
+  } catch (error) {
+    alert("whoops! something went wrong.", error);
+  }
+}
+
+btnRef.addEventListener("click", async () => {
+  btnRef.disabled = true;
+  await videoRef.requestPictureInPicture();
+  btnRef.disabled = false;
+});
+
+// On load
+selectMediaStream();
